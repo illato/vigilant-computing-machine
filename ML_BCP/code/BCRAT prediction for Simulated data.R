4fe6abca37a1a632a9e749f250a20fb2cdb7c0e4
illato <93744440+illato@users.noreply.github.com>
2022-06-07 21:08:40 -0600

    BCRAT - got  last 1/2 of 'BCRAT prediction for Simulated data' running

--- ML_BCP/code/BCRAT prediction for Simulated data.R
+++ ML_BCP/code/BCRAT prediction for Simulated data.R
@@ -76,12 +76,12 @@ AUC(sim_Gail_signal_prediction$Predition_Gail_signal, sim_Gail_signal_prediction
 # table(Predition_Gail_signal_add_NA)
 # sim_Gail_signal_addNA_prediction<-cbind(sim_Gail_signal_add_NA,Predition_Gail_signal_add_NA)
 # 
-# #Calculate AUC 
+# #Calculate AUC
 # sim_GSig_addNA_AUC = confusionMatrix(sim_Gail_signal_addNA_prediction$Case_signalYN, sim_Gail_signal_addNA_prediction$Predition_Gail_signal_add_NA, negative=0)
-# # FP  TP  TN  FN 
-# #381 223 286 310 
+# # FP  TP  TN  FN
+# #381 223 286 310
 # diagnosticErrors(sim_GSig_addNA_AUC)
-# #      acc       sens       spec        ppv        npv        lor 
+# #      acc       sens       spec        ppv        npv        lor
 # #0.4241667  0.4183865  0.4287856  0.3692053  0.4798658 -0.6162081
 # 
 # AUC(sim_Gail_signal_addNA_prediction$Predition_Gail_signal_add_NA, sim_Gail_signal_addNA_prediction$Case_signalYN, label.ordering = NULL)
@@ -109,12 +109,12 @@ AUC(sim_Gail_signal_prediction$Predition_Gail_signal, sim_Gail_signal_prediction
 # table(Predition_Gail_imput1)
 # sim_Gail_imput1_prediction<-cbind(imputed1,Predition_Gail_imput1)
 # 
-# #Calculate AUC 
+# #Calculate AUC
 # sim_Gail_imput1_prediction_AUC = confusionMatrix(sim_Gail_imput1_prediction$Case_signalYN, sim_Gail_imput1_prediction$Predition_Gail_imput1, negative=0)
-# # FP  TP  TN  FN 
-# #359 241 308 292 
+# # FP  TP  TN  FN
+# # 338 264 284 314
 # diagnosticErrors(sim_Gail_imput1_prediction_AUC)
-# #      acc       sens       spec        ppv        npv        lor 
-# #0.4575000  0.4521576  0.4617691  0.4016667  0.5133333 -0.3451795
+# #       acc       sens       spec        ppv        npv        lor
+# # 0.4566667  0.4567474  0.4565916  0.4385382  0.4749164 -0.3475155
 # 
 # AUC(sim_Gail_imput1_prediction$Predition_Gail_imput1, sim_Gail_imput1_prediction$Case_signalYN, label.ordering = NULL)
